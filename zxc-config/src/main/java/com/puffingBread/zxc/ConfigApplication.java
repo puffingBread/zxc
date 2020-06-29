ed8afea41dcb669969d0c29149df3939c04b3614
xuzz <xuzz@yoozoo.com>
2020-06-29 17:22:09 +0800

    test config

--- zxc-config/src/main/java/com/puffingBread/zxc/ConfigApplication.java
+++ zxc-config/src/main/java/com/puffingBread/zxc/ConfigApplication.java
@@ -1,13 +1,11 @@
 package com.puffingBread.zxc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableConfigServer
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
